55d27c2e7838c4dea00620ef0e2d6d3fcd5ce358
Luis Hernández <53943021+luishernand@users.noreply.github.com>
2022-10-11 19:21:04 -0400

    Add files via upload

---
+++ UPDATE.sql
@@ -0,0 +1,22 @@
+--UPDATE
+--SELECT * FROM [dbo].[Orders];
+
+
+--UPDATE [dbo].[Orders]
+--SET order_date = GETDATE()
+--WHERE order_id =2;
+
+
+--SELECT * FROM [dbo].[OrderDetails];
+
+--UPDATE [dbo].[OrderDetails]
+--SET orderDetail_price =5;
+
+
+--SELECT * FROM [dbo].[Products]
+--WHERE product_id=4;
+
+
+--UPDATE [dbo].[Products]
+--SET product_description='Lápiz de puntillas finas'
+--WHERE product_id=4;
